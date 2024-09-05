d382fbb72bbc6bdd9babf0bf11424309510b442e
Dragos Nicu <volutia@gmail.com>
2024-09-05 15:20:15 +0300

    meeergiii

--- config/settings/production.py
+++ config/settings/production.py
@@ -33,7 +33,7 @@ CACHES = {
 # SECURITY
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+#SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
 SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
@@ -60,6 +60,8 @@ SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
     "DJANGO_SECURE_CONTENT_TYPE_NOSNIFF",
     default=True,
 )
+
+CSRF_TRUSTED_ORIGINS = ['68.183.217.100',]
 
 # STATIC & MEDIA
 # ------------------------
