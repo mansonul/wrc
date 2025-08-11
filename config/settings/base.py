9a1715891d3ad04bb9d54ecf133f287787dd2bb3
Dragos Nicu <volutia@gmail.com>
2025-08-11 10:54:54 +0300

    Fixed email

--- config/settings/base.py
+++ config/settings/base.py
@@ -250,7 +250,7 @@ EMAIL_TIMEOUT = 5
 # Django Admin URL.
 ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = [("""WRC""", "wrc@example.com")]
+ADMINS = [("""WRC""", "contact@dragosnicu.com")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 # https://cookiecutter-django.readthedocs.io/en/latest/settings.html#other-environment-settings
